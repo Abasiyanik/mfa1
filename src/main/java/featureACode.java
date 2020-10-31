831d579c03b33264002167b5281ed1de10f8dc59
abasiyanik <abasiyanik@gmail.com>
2020-10-31 13:34:27 -0500

    line 6

--- src/main/java/featureACode.java
+++ src/main/java/featureACode.java
@@ -1,5 +1,7 @@
 public class featureACode {
+
     public static void main(String[] args) {
-        System.out.println("featureA in progreess");
+        System.out.println("halla");
     }
 }
+
--- src/test/java/mfa.java
+++ src/test/java/mfa.java
@@ -10,6 +10,8 @@ public class mfa {
 //line 3
         //line 4
         //line 5
+        //line 6
+        
 
 
 
